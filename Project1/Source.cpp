e5112655fcb503ff45bfbb5168f5582f96351aaa
Taylan Aitken <taitken0010@vbtc.net>
2019-02-11 12:51:15 -0500

    initial commit

---
+++ Project1/Source.cpp
@@ -0,0 +1,13 @@
+#include "SDL.h"
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main(int argc, char *argv[]) {
+	switch (void)
+		case SDL_KEYDOWN; {
+		// spirte x value plus 1
+	}
+	return 0;
+}
